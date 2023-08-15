6e323a82fd878544d0e1e07f28c8d3000b6f21b1
Yazilimpc <tasinsahin@gmail.com>
2023-08-16 01:45:21 +0300

    first commit

---
+++ first_server/index.js
@@ -0,0 +1,16 @@
+const http = require("http")
+const hostname="localhost";
+const port =3000;
+const server = http.createServer((req, res)=>{
+    const {url}=req;
+    console.log(url)
+    if(url==='/translations'){ 
+        const translations={1:'one',2:'two',3:'three'}
+        res.setHeader('Contetn-Type','application/json');
+        res.write(JSON.stringify(translations));
+    }
+    res.end();
+});
+server.listen(port,hostname, ()=>{
+    console.log(`Server running at ${hostname}:${port}`);
+});
